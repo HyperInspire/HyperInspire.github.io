29682ef7ccb1e81126cd17cbe4868f6b99b6f156
Jingyu <tunmxy@163.com>
2025-04-08 20:38:57 +0800

    doc name

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,8 +5,8 @@ import { viteBundler } from '@vuepress/bundler-vite'
 export default defineUserConfig({
   lang: 'en-US',
 
-  title: 'VuePress',
-  description: 'My first VuePress Site',
+  title: 'InspireFace',
+  description: 'InspireFace is a cross-platform face recognition SDK developed in C/C++ that supports multiple operating systems and various inference backends including CPU, GPU, and NPU.',
 
   theme: defaultTheme({
     logo: 'https://vuejs.press/images/hero.png',
